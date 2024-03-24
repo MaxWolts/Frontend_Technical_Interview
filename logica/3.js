468927253af4a994ffd8fb2caaf127a6dc156278
MaxWolts <max.wolts@gmail.com>
2024-03-24 17:41:35 -0300

    :sparkles: Se resuelve el tercer ejercicio de logica

--- logica/3.js
+++ logica/3.js
@@ -11,6 +11,14 @@
    🪣💧🪣🪣💧🪣
    🪣💧🪣🪣💧🪣
    🪣💧🪣🪣🪣🪣
+
+   ---ejemplo---
+         ⏹
+         ⏹
+   ⏹💧💧⏹
+   ⏹💧⏹⏹💧⏹
+   ⏹💧⏹⏹💧⏹
+   ⏹💧⏹⏹⏹⏹
  
    Representando bloque con 🪣︎ y agua con 💧, quedarán atrapadas 7 unidades
    de agua. Suponemos que existe un suelo impermeable en la parte inferior
@@ -18,5 +26,25 @@
  */
 
 const waterBlocks = (value) => {
-  return;
+  let water = 0;
+  let wall = 0;
+  let nextWall = 0;
+
+  for (let index = 0; index < value.length -1; index++) {
+    const blocks = value[index];
+
+    if (nextWall <= blocks) {
+      nextWall = Math.max(...value.slice(index+1));
+    }
+
+    const referenceWall = nextWall > wall ? wall : nextWall;
+    const currentBlocks = referenceWall - blocks;
+    water += currentBlocks >= 0 ? currentBlocks : 0;
+    wall = blocks > wall ? blocks : wall;
+  }
+
+  return water;
 };
+
+//se cambia el 0 del ejemplo ya que se esta trabajando con números enteros positivos segun el enunciado.
+console.log(waterBlocks([4, 1, 3, 6, 1, 3]));
