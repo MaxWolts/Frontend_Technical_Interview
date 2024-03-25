1829e56387e9238fc2ef0f1e3b6b6fd5b529088d
MaxWolts <max.wolts@gmail.com>
2024-03-25 16:17:32 -0300

    :sparkles: Se añade solucion al cuarto ejercicio de logica

--- logica/4.js
+++ logica/4.js
@@ -2,3 +2,32 @@
 //Pista, el codigo presentado handlea dos diferentes tipos de tokens (api token, session token)
 
 import image from "./code_example.png";
+/*
+  El código de la imagen es un layout que maneja aspectos de:
+   - Gestión del estado de la sesión del usuario.
+   - configuración de la moneda.
+   - recuperación de datos.
+
+
+  Tiene funciones encargadas de:
+   - Realizar un inicio de sección y almacenar el token de autenticación en local.
+   - Establecer automáticamente la sessión del usuario si encuentra el token de autenticación.
+   - Refresco del token.
+   - Desconección de la sesión y borrado del token.
+   - Obtención de la información geográfica según la IP.
+   - Obtener datos de los productos.
+   - Establecimiento de la moneda según datos de la sesión.
+   - Actualización de la moneda seleccionada.
+
+
+  Lo que más salta a la vista sería dividir el código según su funcionalidad. De esta manera, luego se importaría al layout dejando su código más claro. 
+  Por ejemplo:
+   - auth.js con las funciones relacionadas con la autentificación.
+   - api.js las referentes a obtención de data y productos.
+   . geolocation.js para las de localización.
+   - currency.js para la gestión de moneda.
+
+   - Eliminar el código comentado (no comentario) y evitar los console.log(error) en el try catch ya que puede haber información sensible.
+   - Respetar el CamelCase (setuserSession tendría que ser setUserSession)
+   - Recomiendo nombres más claros y como de "getToken()" a "fetchAuthToken()"
+*/
